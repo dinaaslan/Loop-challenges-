e3d138aa4c186e2d58d57252d5e7817bfa95a402
Dina Aslan <Dinag.r.aslan@gmail.com>
2026-05-29 19:09:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+for(i=1; i<=20; i++){
+    if(i%2 !==0) {
+        console.log(i);
+    }
+}
+
+for(i=100; i>=0; i--){
+    if(i%3===0){
+        console.log(i);
+    }
+}
+
+for(i=4; i>=-3.5; i-=1.5){
+    console.log(i);
+}
+
+let sum= 0;
+for(i=1; i<=100; i++){
+    sum=sum+i; 
+}
+console.log(sum);
+
+let product=1
+for(i=1; i<=12; i++){
+    product= product*i;
+}
+console.log(product);
